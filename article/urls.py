f29be0aac61dfe9be64b1379d9d423bdb655b634
Brice Leroy <bbrriiccee@gmail.com>
2010-01-13 17:23:30 -0800

    Add article archive, the homepage is almost finish

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('article.views',
     url(r'view/(?P<article_id>\d+)/$', 'article_view', name='article_view'),
     url(r'home/$', 'homepage', name='homepage'),
+    url(r'archive/$', 'article_archive_view', name='article_archive_view'),
     )
  
--- article/views.py
+++ article/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from common.shortcuts import render_response
 
 from models import Article, FeaturedArticle
+
+def paginate(records, request):
+    page = request.GET.get('page', '1')
+    paginator = Paginator(records, 6)
+ 
+    try:
+        page = int(page)
+    except ValueError:
+        page = 1
+ 
+    try:
+        records = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        records = paginator.page(paginator.num_pages)
+    return records
 
 def homepage(request):
     """
@@ -14,16 +30,26 @@ def homepage(request):
         featured = featured[0]
         
     articles = Article.objects.all().select_related().order_by('-creation_date')[:4]
-    context = {'featured': featured,
+    context = {'current': 'home',
+               'featured': featured,
                'articles': articles}
     return render_response(request, 'homepage.html', context)
+
+def article_archive_view(request):
+    articles = Article.objects.filter(is_visible=True).order_by("-modification_date")
+
+    articles = paginate(articles, request)
+    context = {'articles': articles,
+               'current':'archive'}
+    return render_response(request, 'article/archive.html', context)
+
 
 def article_view(request, article_id):
     """
     Show the article # article_id if is_visible is True, overwise raise HTTP404 exception.
     """
     article = get_object_or_404(Article, pk=article_id, is_visible=True)
-    comments = article.comments.select_related().filter(sensored=False)
-    context = {'user_form': article,
+    comments = article.comments.select_related().exclude(status='censored')
+    context = {'article': article,
                'current':'blog'}
-    return render_response(request, 'articles/article_view.html', context)
+    return render_response(request, 'article/article_view.html', context)
